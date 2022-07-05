7268171a1392b808fba8e004022d3a796d15f5b1
jagapatr <jagapatr@motorola.com>
2022-07-05 10:43:30 -0300

    Ultimate evil version

--- EXMGetData/Form1.Designer.cs
+++ EXMGetData/Form1.Designer.cs
@@ -38,12 +38,16 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxDir = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxFile = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(585, 511);
+            this.button1.Location = new System.Drawing.Point(494, 568);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(39, 23);
             this.button1.TabIndex = 0;
@@ -56,7 +60,7 @@
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(12, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(600, 170);
+            this.pictureBox1.Size = new System.Drawing.Size(511, 170);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -65,9 +69,9 @@
             // 
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(179, 474);
+            this.button2.Location = new System.Drawing.Point(174, 544);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(271, 47);
+            this.button2.Size = new System.Drawing.Size(178, 47);
             this.button2.TabIndex = 2;
             this.button2.Text = "GET INFO START";
             this.button2.UseVisualStyleBackColor = true;
@@ -79,7 +83,7 @@
             this.textBoxAllInfo.Location = new System.Drawing.Point(12, 201);
             this.textBoxAllInfo.Multiline = true;
             this.textBoxAllInfo.Name = "textBoxAllInfo";
-            this.textBoxAllInfo.Size = new System.Drawing.Size(601, 266);
+            this.textBoxAllInfo.Size = new System.Drawing.Size(511, 282);
             this.textBoxAllInfo.TabIndex = 3;
             // 
             // label1
@@ -95,7 +99,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(1, 522);
+            this.label2.Location = new System.Drawing.Point(0, 579);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(129, 12);
             this.label2.TabIndex = 5;
@@ -103,7 +107,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 473);
+            this.button3.Location = new System.Drawing.Point(0, 550);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(76, 26);
             this.button3.TabIndex = 6;
@@ -113,7 +117,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(570, 12);
+            this.button4.Location = new System.Drawing.Point(477, 12);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(32, 33);
             this.button4.TabIndex = 7;
@@ -127,12 +131,51 @@
             this.label3.Size = new System.Drawing.Size(0, 13);
             this.label3.TabIndex = 8;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(9, 492);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Directory:";
+            // 
+            // textBoxDir
+            // 
+            this.textBoxDir.Location = new System.Drawing.Point(79, 489);
+            this.textBoxDir.Name = "textBoxDir";
+            this.textBoxDir.Size = new System.Drawing.Size(444, 20);
+            this.textBoxDir.TabIndex = 10;
+            this.textBoxDir.Text = "V:\\\\temp\\\\EXM_INFOS";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(9, 518);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "File Name:";
+            // 
+            // textBoxFile
+            // 
+            this.textBoxFile.Location = new System.Drawing.Point(79, 515);
+            this.textBoxFile.Name = "textBoxFile";
+            this.textBoxFile.Size = new System.Drawing.Size(444, 20);
+            this.textBoxFile.TabIndex = 12;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.ClientSize = new System.Drawing.Size(624, 533);
+            this.ClientSize = new System.Drawing.Size(536, 593);
+            this.Controls.Add(this.textBoxFile);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxDir);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -164,6 +207,10 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxDir;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxFile;
     }
 }
 
--- EXMGetData/Form1.cs
+++ EXMGetData/Form1.cs
@@ -7,14 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using Ivi.Visa.Interop; //EXM commands
-using System.Threading;
+using System.Threading; //thread.Sleep()
+using System.IO; // files.. folder ..etc
 
 namespace EXMGetData
 {
     public partial class Form1 : Form
     {
         private Ivi.Visa.Interop.FormattedIO488 ioTestSet;
- 
+
 
         public Form1()
         {
@@ -33,17 +34,18 @@ namespace EXMGetData
                 ioTestSet = new FormattedIO488();
                 this.textBoxAllInfo.Text = "Starting connection.... \r\n";
                 ResourceManager grm = new ResourceManager();
-                ioTestSet.IO = (IMessage)grm.Open("AGILENT_EXT", AccessMode.NO_LOCK, 2000, "");            
-                
+                ioTestSet.IO = (IMessage)grm.Open("AGILENT_EXT", AccessMode.NO_LOCK, 2000, "");
+
             }
-            catch           
+            catch
             {
                 ioTestSet = null;
                 MessageBox.Show("Falha ao conectar com equipamento EXM!!!");
             }
 
 
-            if (ioTestSet != null) {
+            if (ioTestSet != null)
+            {
 
                 button4.BackColor = Color.Green;
                 label3.Text = "connected!!";
@@ -69,8 +71,8 @@ namespace EXMGetData
                 //textBoxAllInfo.Text += "Command: *OPT? \r\n";
                 ioTestSet.WriteString("*OPT?", true);
                 textBoxAllInfo.Text += "-->LICENSE: " + ioTestSet.ReadString() + "\r\n\r\n";
-    
-            
+
+
                 Thread.Sleep(200);
 
                 //textBoxAllInfo.Text += "Command: :SYSTem:LICense:MODule:HID? \r\n";
@@ -88,11 +90,38 @@ namespace EXMGetData
                 //textBoxAllInfo.Text += "Command: :SYSTem:CONFigure:HARDware? \r\n";
                 ioTestSet.WriteString(":SYSTem:CONFigure:HARDware?", true);
                 textBoxAllInfo.Text += ioTestSet.ReadString() + "\r\n";
-                
 
-           
+                string dir = string.Empty;
+                string fileName = string.Empty;
+
+                dir = textBoxDir.Text;
+                fileName = textBoxFile.Text;
+
+                if (fileName == "")
+                {
+                    MessageBox.Show("!!!!Preencha o campo File Name!!!");
+                }
+                else
+                {
+                    if (System.IO.Directory.Exists(dir))
+                    {
+                        //do nothing
+                    }
+                    else
+                    {
+                        Directory.CreateDirectory(dir);
+                        
+                    }
+
+                    File.WriteAllText(dir + "\\" + fileName, textBoxAllInfo.Text);
+
+                }
+
+
             }
         }
+
+
 
         private void button3_Click(object sender, EventArgs e)
         {
