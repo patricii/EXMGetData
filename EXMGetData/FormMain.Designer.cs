d8698dd0151c49c2f210ee46605a09d6b0db70ba
A. Patricio <thqv74@gmail.com>
2023-06-12 16:04:00 -0300

    First commit

---
+++ EXMGetData/FormMain.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace EXMGetData
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonGetInfo = new System.Windows.Forms.Button();
+            this.textBoxAllInfo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonLed = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxDir = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxFile = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(494, 568);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(39, 23);
+            this.buttonExit.TabIndex = 0;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(511, 170);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonGetInfo
+            // 
+            this.buttonGetInfo.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonGetInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGetInfo.Location = new System.Drawing.Point(174, 544);
+            this.buttonGetInfo.Name = "buttonGetInfo";
+            this.buttonGetInfo.Size = new System.Drawing.Size(178, 47);
+            this.buttonGetInfo.TabIndex = 2;
+            this.buttonGetInfo.Text = "GET INFO START";
+            this.buttonGetInfo.UseVisualStyleBackColor = true;
+            // 
+            // textBoxAllInfo
+            // 
+            this.textBoxAllInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxAllInfo.Location = new System.Drawing.Point(12, 201);
+            this.textBoxAllInfo.Multiline = true;
+            this.textBoxAllInfo.Name = "textBoxAllInfo";
+            this.textBoxAllInfo.Size = new System.Drawing.Size(511, 282);
+            this.textBoxAllInfo.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 185);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "EXM INFORMATION:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(0, 579);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Developed by A. Patricio 2022";
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(0, 550);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(76, 26);
+            this.buttonClear.TabIndex = 6;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonLed
+            // 
+            this.buttonLed.Location = new System.Drawing.Point(477, 12);
+            this.buttonLed.Name = "buttonLed";
+            this.buttonLed.Size = new System.Drawing.Size(32, 33);
+            this.buttonLed.TabIndex = 7;
+            this.buttonLed.UseVisualStyleBackColor = true;
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(399, 185);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(0, 13);
+            this.labelStatus.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(9, 492);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Directory:";
+            // 
+            // textBoxDir
+            // 
+            this.textBoxDir.Location = new System.Drawing.Point(79, 489);
+            this.textBoxDir.Name = "textBoxDir";
+            this.textBoxDir.Size = new System.Drawing.Size(444, 20);
+            this.textBoxDir.TabIndex = 10;
+            this.textBoxDir.Text = "V:\\\\temp\\\\EXM_INFOS";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(9, 518);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "File Name:";
+            // 
+            // textBoxFile
+            // 
+            this.textBoxFile.Location = new System.Drawing.Point(79, 515);
+            this.textBoxFile.Name = "textBoxFile";
+            this.textBoxFile.Size = new System.Drawing.Size(444, 20);
+            this.textBoxFile.TabIndex = 12;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(536, 593);
+            this.Controls.Add(this.textBoxFile);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxDir);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonLed);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxAllInfo);
+            this.Controls.Add(this.buttonGetInfo);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonExit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EXM Get Data";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonGetInfo;
+        private System.Windows.Forms.TextBox textBoxAllInfo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonLed;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxDir;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxFile;
+    }
+}
+
---
+++ EXMGetData/FormMain.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Ivi.Visa.Interop; //EXM commands
+using System.Threading; //thread.Sleep()
+using System.IO; // files.. folder ..etc
+
+namespace EXMGetData
+{
+    public partial class FormMain : Form
+    {
+        private Ivi.Visa.Interop.FormattedIO488 ioTestSet;
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+        private void buttonGetInfo_Click(object sender, EventArgs e)
+        {
+            string dir = string.Empty;
+            string fileName = string.Empty;
+            ioTestSet = new FormattedIO488();
+            ResourceManager grm = new ResourceManager();
+            bool status = false;
+            string errorMessage = "Erro ao conectar com equipamento EXM!!!";
+            try
+            {
+                this.textBoxAllInfo.Text = "Starting connection.... \r\n";
+                ioTestSet.IO = (IMessage)grm.Open("AGILENT_EXT", AccessMode.NO_LOCK, 2000, "");
+            }
+            catch
+            {
+                ioTestSet = null;
+                MessageBox.Show(errorMessage);
+            }
+
+            if (ioTestSet != null)
+            {
+                buttonLed.BackColor = Color.Green;
+                labelStatus.Text = "connected!!";
+
+                status = sendCmd("*IDN?", "-->EXM SERIAL: ");
+                if(status)
+                    status = sendCmd(":SYSTem:MODule:NAME?", "-->MODULE Nº: ");
+
+                if (status)
+                    status = sendCmd(":SYSTem:MODule:SERial?","-->MODULE SERIAL: ");
+
+                if (status)
+                    status = sendCmd("*OPT?","-->LICENSE: ");
+
+                if (status)
+                    status = sendCmd(":SYSTem:LICense:MODule:HID?","-->");
+
+                if (status)
+                    status = sendCmd("INST:CAT?","-->OPTIONS (TDD/FDD): ");
+
+                if (status)
+                    status = sendCmd(":SYSTem:CONFigure:HARDware?","");
+                if (status)
+                {
+                    dir = textBoxDir.Text;
+                    fileName = textBoxFile.Text;
+
+                    if (fileName == "")
+                        MessageBox.Show("!!!!Preencha o campo File Name!!!");
+
+                    else
+                    {
+                        if (System.IO.Directory.Exists(dir))
+                        {
+                            //do nothing
+                        }
+                        else
+                        {
+                            Directory.CreateDirectory(dir);
+                        }
+
+                        File.WriteAllText(dir + "\\" + fileName, textBoxAllInfo.Text);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show(errorMessage);
+                }
+            }
+        }
+        private bool sendCmd(string cmd, string msg)
+        {
+            try
+            {
+                ioTestSet.WriteString(cmd, true);
+                textBoxAllInfo.Text += msg + ioTestSet.ReadString() + "\r\n\r\n";
+                Thread.Sleep(200);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxAllInfo.Clear();
+        }
+
+    }
+}
